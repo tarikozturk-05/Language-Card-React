6316f6d48997719d2d4478219daede2cda66db90
Tarik <a.tarik2005@gmail.com>
2022-10-14 13:13:13 +0300

    bittii

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Card from './components/Card/Card';
 import items from './helper/data';
+import logo from './assets/react.svg'
 
 function App() {
   return (
     
-       <div>
+       <div >
+        <img  className='logo' src={logo} alt="" />
       <Card items={items} />
        </div>  
 
